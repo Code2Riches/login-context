4b89f79c336fbe71081159dcdbb78f7ff63b6e95
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-04-03 13:10:23 -0700

    added more components to show how context distributes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home'
 import { ThemeContext } from './context/ThemeContext';
+import Card from './components/Card/Card';
+import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
   const [theme, setTheme] = useState('dark')
+  //global theme setter
+  const changeTheme = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light') 
+  }
   let words = 'Yo!'
   return (
     <div className="App">
       <ThemeContext.Provider value={
-        {theme, setTheme, words}
+        {theme, setTheme, words, changeTheme}
       }>
         <h1>Login</h1>
+        <NavBar />
         <Home />
       </ThemeContext.Provider>
+      <Card />
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,26 @@
 import React, {useContext} from 'react'
 import {ThemeContext} from '../../context/ThemeContext';
 import './Home.css'
+import Card from '../Card/Card';
 
 const Home = () => {
-  const {theme, setTheme, words} = useContext(ThemeContext)
+  const {theme, setTheme, words, changeTheme} = useContext(ThemeContext)
   const funcAlert = () => {
     alert(words)
   }
+// local theme setter
+//   const changeTheme = () => {
+//     let currentTheme = theme
+//     currentTheme === 'light' ? setTheme('dark') : setTheme('light') 
+//   }
   return (
       <>
       <div className={theme}>Home</div>
-      <button onClick={() => setTheme('light')}>Change Theme</button>
+      <button onClick={() => changeTheme()}>Change Theme</button>
       <p>{theme}</p>
       <p>{words}</p>
       <button onClick={()=>funcAlert()}>Funk</button>
+      <Card />
       </>
   )
 }
