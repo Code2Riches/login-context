c95a69846b7dd804e1c9ce5587ef012cdb548ef9
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-04-04 08:22:35 -0700

    Added Login context

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import {createContext, useContext, useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { ThemeContext } from './context/ThemeContext';
+import { LoginContext, LoginProvider } from './context/LoginContext';
 
 function App() {
   const [theme, setTheme] = useState('dark')
-  //global theme setter
  
   return (
     <div className="App">
       <ThemeContext.Provider value={
         {theme, setTheme}
       }>
-        <h1>Login</h1>
-
+        <LoginProvider>
+          <h1>Login</h1>
+          <p>Yo!</p>
+        </LoginProvider>
       </ThemeContext.Provider>
     </div>
   );
