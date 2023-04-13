a12e778cb28d5eed491107710c73932ad974ef2b
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-04-13 08:32:03 -0700

    token now passing to headers

--- src/lib/checkAuthToken.js
+++ src/lib/checkAuthToken.js
@@ -1,13 +1,15 @@
+import { setHeaderToken } from "./setHeaderToken"
+
 export const checkAuthToken = () => {
    
     let jwtToken = localStorage.getItem('jwtToken')
 
     if (jwtToken) {
-        // set auth to false
-        alert("True")
+        // set auth to true
+        setHeaderToken(jwtToken)
     } else {
         //set auth to false
-        alert("False")
+        
     }
     
 }
